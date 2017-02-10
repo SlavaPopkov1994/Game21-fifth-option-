406d1505499cf5904004865f531d9d1359c57df8
Slava <slava.popkov.1994@gmail.com>
2017-02-10 15:29:19 +0300

    Update CardEnumeration.cs

--- CardEnumeration.cs
+++ CardEnumeration.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
-    class CardEnumeration
+    public class CardEnumeration
     {
         public enum colorsCards { hearts = 0, diamonds = 1, spades = 2, clubs = 3};
         public enum valuesCasrds { jack = 0, queen = 1, king = 2, ace = 3};
